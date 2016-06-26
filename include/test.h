d6bd6b3cea701633cde71d5229b63bf048cf137c
Anthony <anthondw@usc.edu>
2016-06-25 20:13:05 -0700

    First commit

---
+++ include/test.h
@@ -0,0 +1,5 @@
+/**
+ * This function takes two numbers
+ * and returns their sum.
+**/
+int add_nums(int a, int b);
---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../test.h";
+
+int add_nums(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int x = 2;
+	int y = 3;
+
+	printf("The sum of x and y is %d\n", add_nums(x,y));
+
+	return 0;
+}
